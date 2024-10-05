9728f70e92546c56b4c83df71e52636d64960831
Jay Wood <jwoodcodes@gmail.com>
2024-10-05 16:15:01 -0500

    d6

--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,18 @@
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
-// import GetData from "./data/dataHandler";
-import { initialSleeperPlayerData, captainData } from "./data/dataHandler";
-import AppContainer from "./components/AppContainer";
-import PlayerSelect from "./components/playerSelect";
-import UserAuth from "./components/UserAuth";
-import oCaptainLeagueDataArray from "./data/oCaptainLeagueDataArray";
-export default function Home() {
-  const [initialSleeperPlayerData, setInitialSleeperPlayerData] = useState([]);
+import PlayerSelectWrapper from './components/playerSelectWrapper';
 
-  useEffect(() => {
-    async function fetchSleeperData() {
-      try {
-        const response = await fetch('/api/sleeper-players');
-        if (!response.ok) {
-          throw new Error('Failed to fetch sleeper player data');
-        }
-        const data = await response.json();
-        setInitialSleeperPlayerData(data);
-      } catch (error) {
-        console.error('Error fetching sleeper player data:', error);
-      }
-    }
-
-    fetchSleeperData();
-  }, []);
-
-  // console.log(initialSleeperPlayerData);
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black">
-      <h1 className="text-sky-400 text-6xl mb-20 ">Captain Tracker 2024</h1>
-      {/* <UserAuth />
-      <PlayerSelect initialSleeperPlayerData={initialSleeperPlayerData} /> */}
-      <AppContainer
-        initialSleeperPlayerData={initialSleeperPlayerData}
-        captainData={captainData}
-      />
-      <PlayerSelect
-        initialSleeperPlayerData={initialSleeperPlayerData}
-        user={user}
-        week={week}
-        captainData={captainData}
-        setCaptainDataState={setCaptainDataState}
-      />
-    </main>
-  );
+async function getData() {
+  // Fetch your initial data here
+  // ...
+  return {
+    initialSleeperPlayerData,
+    user,
+    week,
+    captainData,
+  };
 }
 
-// or wherever PlayerSelect is being used
-console.log('Rendering parent component with props:', {
-  initialSleeperPlayerDataLength: initialSleeperPlayerData?.length,
-  user,
-  week,
-  captainDataLength: captainData?.length
-});
+export default async function Home() {
+  const data = await getData();
 
-// ... other code ...
-
-// npm --max-old-space-size=8192 run dev
+  return <PlayerSelectWrapper {...data} />;
+}

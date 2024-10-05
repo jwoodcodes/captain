0ee3f9965dd660bfdee870fdd9bc527deb217829
Jay Wood <jwoodcodes@gmail.com>
2024-10-05 17:39:41 -0500

    pls d2

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,4 @@
 import PlayerSelectWrapper from './components/playerSelectWrapper';
-<<<<<<< HEAD
-import { handleUpdateTable } from './components/ClientPlayerSelect';
-async function getData() {
-  // Fetch your initial data here
-  // This is just an example, replace with your actual data fetching logic
-  const initialSleeperPlayerData = []; // Fetch this data
-  const user = {}; // Fetch user data
-  const week = 1; // Set the current week
-  const captainData = []; // Fetch captain data
-
-=======
 
 async function getData() {
   // Fetch your initial data here
@@ -18,8 +7,18 @@ async function getData() {
   const user = {}; // Fetch user data
   const week = 1; // Set the current week
   const captainData = []; // Fetch captain data
+}
 
->>>>>>> parent of 1a2b84f (d16)
+
+async function getData() {
+  // Fetch your initial data here
+  // This is just an example, replace with your actual data fetching logic
+  const initialSleeperPlayerData = []; // Fetch this data
+  const user = {}; // Fetch user data
+  const week = 1; // Set the current week
+  const captainData = []; // Fetch captain data
+
+
   return {
     initialSleeperPlayerData,
     user,

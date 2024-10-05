de1ed3266cd0aea913e6ac6d506dbaf27f7101e3
Jay Wood <jwoodcodes@gmail.com>
2024-10-05 17:13:14 -0500

    Revert "d16"
    This reverts commit 1a2b84f8b6914817dcc1436ed2d4c3c525cc7add.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,25 @@
-import ErrorBoundary from './components/ErrorBoundary';
-import YourServerComponent from './components/YourServerComponent';
+import PlayerSelectWrapper from './components/playerSelectWrapper';
 
-export default function Page() {
-  return (
-    <ErrorBoundary>
-      {(setError) => (
-        <YourServerComponent setError={setError} />
-      )}
-    </ErrorBoundary>
-  );
+async function getData() {
+  // Fetch your initial data here
+  // This is just an example, replace with your actual data fetching logic
+  const initialSleeperPlayerData = []; // Fetch this data
+  const user = {}; // Fetch user data
+  const week = 1; // Set the current week
+  const captainData = []; // Fetch captain data
+
+  return {
+    initialSleeperPlayerData,
+    user,
+    week,
+    captainData,
+  };
+}
+
+
+
+export default async function Home() {
+  const data = await getData();
+
+  return <PlayerSelectWrapper {...data} />;
 }

44e568f74686d5de17fa65624c6a38b43414ddc0
Jay Wood <jwoodcodes@gmail.com>
2024-10-15 16:02:14 -0500

    d

--- src/app/data/kickoffTimes.js
+++ src/app/data/kickoffTimes.js
@@ -9,7 +9,7 @@ const kickoffTimes = [
   {
     team: "ATL",
     date: "10-20-2024",
-    // the above is in the format of "MM-DD-YYYY"
+    
     kick: "12:00",
     //the above is (in 24-hour format, e.g., "14:30" for 2:30 PM)
   },

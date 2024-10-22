a9c7bc518214bb690a4944f9322be13c2072153e
Jay Wood <jwoodcodes@gmail.com>
2024-10-22 15:49:15 -0500

    week 8 kickoff update

--- src/app/components/AppContainer.js
+++ src/app/components/AppContainer.js
@@ -9,7 +9,7 @@ export default function AppContainer({
   captainData,
 }) {
   const [user, setUser] = useState(null);
-  const [week, setWeek] = useState(7);
+  const [week, setWeek] = useState(8);
   const [captainDataState, setCaptainDataState] = useState(captainData);
 
   return (
